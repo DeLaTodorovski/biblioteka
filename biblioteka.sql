89387a6c10f7746ac48a6e2b9f47a6eaaa39285c
Daniel <daniel.likovno@gmail.com>
2025-01-12 20:39:05 +0100

    New database file added

--- biblioteka.sql
+++ biblioteka.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2025 at 08:37 PM
+-- Generation Time: Jan 12, 2025 at 08:38 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
